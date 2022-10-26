4ff8a7003975e075a9380339031f912a6cd6e2a9
0xbanks <116527130+0xbanks@users.noreply.github.com>
2022-10-26 12:15:17 -0400

    Add files via upload

---
+++ fastMint.js
@@ -0,0 +1,50 @@
+const express = require('express')
+var parseUrl = require('body-parser')
+
+const API_KEY = 'sk_live_3c0c77fe-5b9e-4ec6-bdfd-f9698998d717';
+const app = express()
+let encodeUrl = parseUrl.urlencoded({ extended: false })
+
+app.get('/image', (req, res) => {
+  res.sendFile(__dirname + '/form.html')
+})
+
+app.post('/image', encodeUrl, (req, res) => { 
+  
+  console.log('Request body:', req.body) 
+  const sdk = require('api')('@verbwire/v1.0#hr2s143dl9hbr7s9');
+
+sdk.auth(API_KEY);
+sdk.post('/nft/mint/quickMintFromFile', {
+  allowPlatformToOperateToken: req.body.allowPlatformToOperateToken,
+  chain: req.body.chain, 
+  filePath:  req.body.Image, 
+  name: req.body.name,
+  description: req.body.name,
+  recipientAddress: req.body.recipientAddress,
+}, {accept: 'application/json'})
+  .then(resp => res.send(resp))
+  .catch(err => console.error(err)); 
+})
+
+app.get('/url', (req, res) => {
+    res.sendFile(__dirname + '/form2.html')
+  })
+  
+  app.post('/url', encodeUrl, (req, res) => {
+    console.log('Form request:', req.body)
+   
+	const sdk = require('api')('@verbwire/v1.0#hr2s143dl9hbr7s9');
+  
+  sdk.auth(API_KEY);
+  sdk.post('/nft/mint/quickMintFromMetadataUrl', {
+    allowPlatformToOperateToken: req.body.allowPlatformToOperateToken,
+    chain: req.body.chain,
+    metadataUrl: req.body.url,
+    description: req.body.name,
+    recipientAddress: req.body.recipientAddress,
+  }, {accept: 'application/json'})
+    .then(resp => res.send(resp))
+    .catch(err => console.error(err)); 
+  })
+app.listen(8080)
