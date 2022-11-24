92d3bae6bc31715196675d3f848d3f368c99d0e0
0xbanks <116527130+0xbanks@users.noreply.github.com>
2022-11-24 02:41:55 -0500

    Add files via upload

--- fastMint.js
+++ fastMint.js
@@ -5,26 +5,51 @@ const API_KEY = 'YOUR API KEY GOES HERE';
 const app = express()
 let encodeUrl = parseUrl.urlencoded({ extended: false })
 
+const axios = require('axios');
+const fs = require('fs');
+const FormData = require('form-data');
+
+
+var multipart = require('connect-multiparty')
+var multipartmiddleware = multipart();
+
 app.get('/image', (req, res) => {
   res.sendFile(__dirname + '/form.html')
 })
 
-app.post('/image', encodeUrl, (req, res) => { 
+app.post('/image',multipartmiddleware, (req, res) => { 
   
-  console.log('Request body:', req.body) 
-  const sdk = require('api')('@verbwire/v1.0#hr2s143dl9hbr7s9');
 
-sdk.auth(API_KEY);
-sdk.post('/nft/mint/quickMintFromFile', {
-  allowPlatformToOperateToken: req.body.allowPlatformToOperateToken,
-  chain: req.body.chain, 
-  filePath:  req.body.Image, 
-  name: req.body.name,
-  description: req.body.name,
-  recipientAddress: req.body.recipientAddress,
-}, {accept: 'application/json'})
-  .then(resp => res.send(resp))
-  .catch(err => console.error(err)); 
+// axios
+var data = new FormData();
+data.append('allowPlatformToOperateToken', req.body.allowPlatformToOperateToken);
+data.append('chain', req.body.chain);
+data.append('recipientAddress', req.body.recipientAddress);
+data.append('filePath', fs.createReadStream(req.files.filePath.path));
+data.append('name', req.body.name);
+data.append('description', req.body.description);
+
+var config = {
+  method: 'post',
+  url: 'https://api.verbwire.com/v1/nft/mint/quickMintFromFile',
+  headers: { 
+    'X-API-Key': API_KEY, 
+    ...data.getHeaders()
+  },
+  data : data
+};
+
+console.log("data",data);
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+  res.send(JSON.stringify(response.data))
+})
+.catch(function (error) {
+  console.log(error);
+});
+
 })
 
 app.get('/url', (req, res) => {
